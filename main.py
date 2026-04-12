0e2bb1c18a04b867badfde7ddef6e6f50f52bbc6
elvishuges <elvishuges@hotmail.com>
2026-04-11 22:48:36 -0300

    primeira comunicacao com modelo OPENAI

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_openai import ChatOpenAI
 
-print("Hello, VS Code!")
-def greet(name):
-    return f"Hello, {name}!"
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    print(greet(name))
+
+load_dotenv()
+chave_api = os.getenv("OPENAI_API_KEY")
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="What is the capital of France?"),
+]
+modelo_version = "gpt-4o"
+modelo = ChatOpenAI(model=modelo_version) 
+response = modelo.invoke(messages)
+print(response.content)
